38ccc4f29be5a35ff7a19aac86815f8720bc3faf
zachclem42@gmail.com <ZCC@ZachsMachine.fios-router.home>
2018-03-04 11:36:03 -0500

    clean up some unused code in methods

--- client/js/events.js
+++ client/js/events.js
@@ -30,31 +30,3 @@ Template.events.events({
 		Bert.alert("Your are attending this event", "success", "growl-top-right");
 	}
 });
-
-	// this is where 'attend event function will be'
-	// each event needs submit button
-
-		/* "click #laugh": function() {
-			var thisUser = Meteor.userId();
-			var thisEvent = Events.findOne({_id: this._id})._id;
-			var eventAuthor = Events.findOne({_id: this._id}).userId;
-			var Name = Meteor.user().username;
-			var thisEventsVotes = Events.findOne({_id: this._id}, {voted: {$in: Name}}).voted;
-	
-			if (thisEventsVotes.indexOf(Name) > -1) {
-				Bert.alert("You cannot vote twice", "danger", "growl-top-right");
-				// In the array!, meaning user has voted
-			} else {
-				// Not in the Array, Do stuff.
-				Meteor.call("countVote", thisEvent, Name);
-				Meteor.call("userPointLaugh", eventAuthor);
-				Meteor.call("laughVote", thisUser, thisEvent);
-				Bert.alert("Your Vote Was Placed", "success", "growl-top-right");
-	
-			}
-	
-			if (Name == thisEventsVotes) {
-				Bert.alert("You cannot vote for your own event", "danger", "growl-top-right");
-			}
-		}, });
-		*/
